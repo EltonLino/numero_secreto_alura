4cfa095c2872cc843cf35226ad21ca60d5104a44
Elton Lino <eltonblino@gmail.com>
2025-05-02 07:58:36 -0300

    numero secreto alura

---
+++ app.js
@@ -0,0 +1,64 @@
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto){
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
+        preencherCampo('h1', "Acertou!");
+        preencherCampo("p", `Parabéns você acertou o numero secreto com ${tentativas} ${palavraTentativa}`);
+        chute = document.getElementById("reiniciar").removeAttribute("disabled");
+    }else
+        if(chute > numeroSecreto){
+            preencherCampo("p", "O número secreto é menor");
+        } else{
+            preencherCampo("p", "O número secreto é maior");
+        }
+        tentativas++;
+        limparCampo();
+}
+
+function preencherCampo(tag, texto){
+    let titulo = document.querySelector(tag);
+    titulo.innerHTML = texto;
+    responsiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1.2});
+}
+
+function gerarNumeroAleatorio(){    
+    let quantidadeNumerosNaLista = numerosSorteados.length;
+    if (quantidadeNumerosNaLista == 10){
+        numerosSorteados = [];
+    }
+
+    numeroEscolhido =  parseInt(Math.random() * 10 + 1);
+
+    if (numerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        numerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciar() {
+    limparCampo();
+    numeroSecreto = gerarNumeroAleatorio();
+    tentativas = 1;
+    iniciarJogo();
+    document.getElementById("reiniciar").setAttribute("disabled",true);
+}
+
+function iniciarJogo() {
+    preencherCampo("h1", "Jogo do número secreto");
+    preencherCampo("p", "Escolha um número entre 1 e 10");   
+}
+
+let numerosSorteados = [];
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+iniciarJogo();
+console.log(numerosSorteados);
